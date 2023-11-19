352f8be45985f6e5e692471470ccf2cad716128e
tslee <dlxotjde@gmail.com>
2023-11-19 21:26:20 +0900

    feat: add SiteAuthorityT 로직과 상관 없음

--- src/types/site.ts
+++ src/types/site.ts
@@ -2,6 +2,10 @@ import { SITE_DISPLAY } from '@src/constants/display/SITE_DISPLAY';
 
 export type Sites = keyof typeof SITE_DISPLAY;
 
+export type SiteAuthorityT = {
+  [key in Sites]: boolean;
+};
+
 type SiteInfo = {
   kr: '명동';
   en: 'Myeongdong';
